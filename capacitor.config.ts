26e25ac98d63a7230cd84be603188792cbbdd96f
Yarin-Comunicacao <2.09496789e+08+Yarin-Comunicacao@users.noreply.github.com>
2026-08-16 20:55:30 +0000

    feat: integrate Capacitor for Android and iOS support

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'br.com.avalyarin.app',
+  appName: 'Avalyarin',
+  webDir: 'dist/public'
+};
+
+export default config;
